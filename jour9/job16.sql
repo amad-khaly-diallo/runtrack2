d759760b118fd371d0fdcd808f46e35ebd45ccaa
amad-khaly-diallo <amad-khaly.diallo@laplateforme.io>
2025-07-29 11:56:03 +0200

    fin du projet phpmyadmin et sql jour9

---
+++ jour9/job16.sql
@@ -0,0 +1,9 @@
+SELECT 
+    etages.nom AS nom_etage, 
+    salles.nom AS "Biggest Room", 
+    salles.capacite
+FROM salles
+JOIN etages ON salles.id_etage = etages.id
+WHERE salles.capacite = (
+    SELECT MAX(capacite) FROM salles
+);
